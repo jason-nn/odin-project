53fdb712f3337eb7ff2040e36fd7565af592994f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-05 23:25:14 +0800

    Added logical operator exercises

--- logical-operators.js
+++ logical-operators.js
@@ -14,3 +14,32 @@ if (username === 'Admin') {
 } else {
   alert('Canceled');
 }
+
+let jsname = prompt('What is the “official” name of JavaScript?');
+
+if (jsname === 'ECMAScript') {
+  alert('Right!');
+} else {
+  alert("You don't know? ECMAScript!");
+}
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+message =
+  login === 'Employee'
+    ? 'Hello'
+    : login === 'Director'
+    ? 'Greetings'
+    : login === ''
+    ? 'No login'
+    : '';
