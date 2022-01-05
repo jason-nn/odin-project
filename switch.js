2a5263ab3a826b2aeaada94917ea74537ce3bf39
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-05 23:24:57 +0800

    Add switch examples

--- switch.js
+++ switch.js
@@ -20,3 +20,60 @@ switch (username) {
     console.log('Who are you?');
     break;
 }
+
+// Set the student's grade
+const grade = 87;
+
+switch (true) {
+  // If score is 90 or greater
+  case grade >= 90:
+    console.log('A');
+    break;
+  // If score is 80 or greater
+  case grade >= 80:
+    console.log('B');
+    break;
+  // If score is 70 or greater
+  case grade >= 70:
+    console.log('C');
+    break;
+  // If score is 60 or greater
+  case grade >= 60:
+    console.log('D');
+    break;
+  // Anything 59 or below is failing
+  default:
+    console.log('F');
+}
+
+// Get number corresponding to the current month, with 0 being January and 11 being December
+const month = new Date().getMonth();
+
+switch (month) {
+  // January, February, March
+  case 0:
+  case 1:
+  case 2:
+    console.log('Winter');
+    break;
+  // April, May, June
+  case 3:
+  case 4:
+  case 5:
+    console.log('Spring');
+    break;
+  // July, August, September
+  case 6:
+  case 7:
+  case 8:
+    console.log('Summer');
+    break;
+  // October, November, December
+  case 9:
+  case 10:
+  case 11:
+    console.log('Autumn');
+    break;
+  default:
+    console.log('Something went wrong.');
+}
