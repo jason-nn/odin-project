55b8aaca6ea560b3bea3d7d08a98d20ede19b297
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-05 22:41:01 +0800

    Add switch example

---
+++ switch.js
@@ -0,0 +1,22 @@
+let username = 'Jason';
+username = 'Steve';
+username = 'Sam';
+username = 'Paul';
+
+switch (username) {
+  case 'Jason':
+    console.log('Welcome back, Jason');
+    break;
+
+  case 'Steve':
+    console.log('Welcome bac, Steve');
+    break;
+
+  case 'Sam':
+    console.log('Welcome back, Sam');
+    break;
+
+  default:
+    console.log('Who are you?');
+    break;
+}
