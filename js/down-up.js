90c20435b5f7ce415b2e0d2d1c2e2bb5aa2535ed
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-09 01:52:56 +0800

    Add down up iterative solution

--- js/down-up.js
+++ js/down-up.js
@@ -1,12 +1,27 @@
 // recursion
 
-const downup = (str) => {
+let downup = (str) => {
   if (str.length === 1) {
     return str;
   } else {
     console.log(str);
     downup(str.slice(0, str.length - 1));
     console.log(str);
+  }
+};
+
+downup('jason');
+
+console.log('---');
+
+// iteration
+
+downup = (str) => {
+  for (let i = str.length; i > 0; i--) {
+    console.log(str.slice(0, i));
+  }
+  for (let i = 2; i <= str.length; i++) {
+    console.log(str.slice(0, i));
   }
 };
 
